2432f45edc8906c36800cff21aeb23b5704c2968
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-01 11:09:02 -0300

    react_aula_1

--- intro_react/src/App.js
+++ intro_react/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Erick'
+const lastName = 'Andrade'
+const job = 'Developer'
+const skills = ['HTML, CSS, JavaScript, React']
+const skillsMap = skills.map((hab, index) => <li key={index}>{hab}</li>)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className= "div-flex">
+    <p>{name} {lastName}</p>
+    <p>Profession: {job}</p>
+    <ul>Languages: {skillsMap}</ul>
+  </div>
 }
 
 export default App;
