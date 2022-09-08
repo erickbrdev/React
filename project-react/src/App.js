adc1056e263ce95afed63d02f9f48edf90ce2695
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-08 10:25:22 -0300

    adicionando a funcao dentro da classe

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function handleClick () {
-  console.log('Teste')
-}
 
-let number = 0
-function sum () { 
-
-    for (let index = 0;  index < 50; index += 1)
-    number += index
-    console.log(number)
-}
 
 
 class App extends React.Component {
+   handleClick () {
+    console.log('Teste')
+  }
+  
   render() {
     return (
       <div>
-        <button onClick={handleClick}>Click</button>
-        <button onClick={sum}>Sum</button>
+        <button onClick={this.handleClick}>Click</button>        
       </div>     
     )
   }
