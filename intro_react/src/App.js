1bc59191266cae6aca1133aa5f8f442484d02288
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-16 11:13:53 -0300

    Router

--- intro_react/src/App.js
+++ intro_react/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import HeroTitle from './Components/title';
+import HomePage from './Components/homepage';
+import About from './Components/about';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <BrowserRouter>
+      <h1>React is wonderful</h1>
+      <Route exact path="/" component={HeroTitle} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/about" component={About} />
+      </BrowserRouter>
+    )
+  }
+  
 }
 
 export default App;
---
+++ intro_react/src/Components/title.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HeroTitle extends Component {
+    render() {
+        return (
+            <h1>React  is amazing</h1>
+        )
+    }
+}
+
+export default HeroTitle;
